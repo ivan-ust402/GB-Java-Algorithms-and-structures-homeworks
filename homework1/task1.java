62f5dca3f9a0f3e43dc91e270cbb6014ddf367e8
Roman <roman@MacBook-Pro-Roman.local>
2023-06-05 12:12:11 +0700

    Completed task1

--- homework1/task1.java
+++ homework1/task1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class task1 {
     // Дано: отсортированный массив с повторяющимися элементами
     // например, 0, 0, 1, 1, 1, 2, 2, 3, 3, 4
@@ -5,6 +7,20 @@ public class task1 {
     // Оставшиеся элементы могут быть любыми
     // Должно получится так: 0, 1, 2, 3, 4, - 
     public static void main(String[] args) {
-        
+        int[] array = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        removeDuplicates(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void removeDuplicates(int[] array) {
+        // O(n)
+        int i = 0;
+        for (int j = 1; j < array.length; j++) {
+            if(array[j] != array[i]) {
+                i += 1;
+                array[i] = array[j];
+                System.out.println(array[j]);
+            }
+        }
     }
 }

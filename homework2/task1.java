a7fd3e869362fbf0c832496f98a37cdeec08583c
Roman <roman@MacBook-Pro-Roman.local>
2023-06-05 16:56:44 +0700

    Created counting sort method

--- homework2/task1.java
+++ homework2/task1.java
@@ -1,5 +1,19 @@
 public class task1 {
    public static void main(String[] args) {
-    
+   // Сортировка подсчетом
+   // [1, 2, 3, 4, 5, 2, 3, 4, 5, 3, 4, 5, 4, 5]
+   // 1 -> 1
+   // 2 -> 2
+   // 3 -> 3
+   // 4 -> 4
+   // 5 -> 5
+   // В исходный массив всталяем 1 единицу, 2 двойки, 3 тройки и т.д.
    } 
+
+   public static void countingSort(int[] array) {
+      int max = array[0];
+      for (int i = 0; i < array.length; i++) {
+         
+      }
+   }
 }

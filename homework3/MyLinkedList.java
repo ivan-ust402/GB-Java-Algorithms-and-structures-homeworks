2463f3ca0977c4d19bea6036a9ad63b3c1a85e40
Roman <roman@MacBook-Pro-Roman.local>
2023-06-08 20:34:00 +0700

    Created method add

--- homework3/MyLinkedList.java
+++ homework3/MyLinkedList.java
@@ -3,7 +3,7 @@ package homeworks.homework3;
 public class MyLinkedList {
 
     private Node head;
-    public class Node {
+    private class Node {
         int value;
         Node next;
 
@@ -15,5 +15,29 @@ public class MyLinkedList {
             this.value = value;
             this.next = next;
         }
+    }
+
+    public void add(int value) {
+        // Добавляем в конец нашего списка значение
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            Node last = findLast();
+            last.next = new Node(value);
+        }
+    }
+
+    private Node findLast() {
+        if (head == null) {
+            return null; 
+        }
+
+        Node cursor = head;
+
+        while (cursor.next == null) {
+            cursor = cursor.next;
+        }
+
+        return cursor;
     }
 }

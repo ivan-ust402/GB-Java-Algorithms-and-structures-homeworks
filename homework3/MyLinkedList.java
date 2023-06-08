8fe7bbc8bb5d9e48bc99a4604d0a0a6f3d6b3963
Roman <roman@MacBook-Pro-Roman.local>
2023-06-08 20:03:41 +0700

    Initial commit

---
+++ homework3/MyLinkedList.java
@@ -0,0 +1,14 @@
+public class MyLinkedList {
+    public class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            
+        }
+    }
+}
